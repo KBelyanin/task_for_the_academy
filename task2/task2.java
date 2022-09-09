a6b17268f99877f878d7234a91c70e1350061a9a
Konstantin.Belyanin <Konstantin.Belyanin@russianpost.ru>
2022-09-09 16:29:19 +0400

    third

--- task2/task2.java
+++ task2/task2.java
@@ -7,58 +7,58 @@ class task2 {
             arr[i] = Integer.parseInt(args[i]);
         }
 
-        int runs = 0; // счётчик для решения
-        // флаг для запуска и остановки цикла, является защитой циклов от деления на ноль
+        int runs = 0;
+
         boolean flag = arr.length == 0? true : false;
 
-        int sum = Arrays.stream(arr).sum(); // сумма элементов
+        int sum = Arrays.stream(arr).sum();
 
-        int mid = 0; // искомое для приведения число
+        int mid = 0;
         if (arr.length == 0) {
             System.out.println(0);
         }
-        else if (arr.length % 2 == 1) { // если массив нечётный, придётся искать медиану.
+        else if (arr.length % 2 == 1) {
             mid = (int) findMedian(arr);
-        } else { // если чётный - среднее арифметическое
-            // в чётном массиве не имеет значение в какую сторону будет округление, потому просто вниз через int.
+        } else {
+
             mid = sum / arr.length;
         }
 
         while (!flag) {
-            for (int j = 0; j < arr.length; j++) { //проход по списку с изменением объектов
-                // если число равно, действия не будет, счётчик не увеличивается, ибо он в теле цикла.
+            for (int j = 0; j < arr.length; j++) {
+                
                 if (arr[j] != mid) {
-                    // увеличение/уменьшение числа
+
                     arr[j] = arr[j] < mid ? arr[j] + 1 : arr[j] - 1;
-                    runs++; // увеличение счётчика
+                    runs++;
                 }
             }
-            flag = allEqual(arr, mid); // флаг выхода из цикла
+            flag = allEqual(arr, mid);
         }
-        System.out.println(runs); // вывод результата
+        System.out.println(runs);
     }
 
     public static boolean allEqual(int[] array, int middle) {
         if (array == null || array.length == 0) {
-            return true; // защита от пустого массива
+            return true;
         }
         for (int e : array) {
             if (e != middle) {
-                return false; // хотя бы один элемент не равен
+                return false;
             }
         }
-        return true; // все элементы равны
+        return true;
     }
 
     public static double findMedian(int[] array) {
         if (array == null || array.length == 0) {
-            return 0; // защита от пустого массива
+            return 0;
         }
-        int[] copy = array.clone(); // массив для сортировки
+        int[] copy = array.clone();
         Arrays.sort(copy);
         if (copy.length % 2 == 0) {
             return ((copy[copy.length / 2] + copy[copy.length / 2 - 1]) / 2f);
         }
-        return copy[copy.length / 2]; // вывод медианы
+        return copy[copy.length / 2];
     }
 }
